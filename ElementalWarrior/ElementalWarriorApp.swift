36102df974d222f453cff7d47575f4e14a612f24
Peter Gelgor <peter.gelgor@queensu.ca>
2025-12-30 12:09:33 -0500

    Initial Commit

---
+++ ElementalWarrior/ElementalWarriorApp.swift
@@ -0,0 +1,33 @@
+//
+//  ElementalWarriorApp.swift
+//  ElementalWarrior
+//
+//  Created by Peter Gelgor on 2025-12-30.
+//
+
+import SwiftUI
+
+@main
+struct ElementalWarriorApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
